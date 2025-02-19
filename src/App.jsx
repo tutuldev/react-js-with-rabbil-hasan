013bf5d148575d8630494db6c9e4efbf4737c056
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-19 22:29:47 +0600

    jsx vs html differents

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,14 @@ const App = () => {
        <Hero/>
        <ContactForm/>
        <Footer/>
+       {/* self close  */}
+       <button/>
+       {/* class  */}
+       <h1 className="">Hellow</h1>
+       {/* attrubute need camel case */}
+       <input onSubmit="" />
+       {/* inline css  */}
+       <p style={{color:'red'}}></p>
     </div>
   );
 };
