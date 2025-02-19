e596273ea37e3d332225ddb4ca99dc87878dd753
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-19 23:08:35 +0600

    jsx loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
 // rsc for react functional component 
 
-import ContactForm from "./components/ContactHome";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
 
 const App = () => {
-let marks=90;
+  const city =['Dhaka','London','Delhi','Kolkata']
   return (
     <div>
-       <Header/>
-       <Hero/>
-       <ContactForm/>
-       <Footer/>
-       {/* self close  */}
-       <button/>
-       {/* class  */}
-       <h1 className="">Hellow</h1>
-       {/* attrubute need camel case */}
-       <input onSubmit="" />
-       {/* inline css  */}
-       <p style={{color:'red'}}></p>
-      {/* if else  */}
-      {/* {marks>80?<h1>Brilliant</h1>:<h1>Average</h1>} */}
-
-      {/* imaditly invocable  */}
+       <ul>
+            {
+             city.map((item,i)=>{
+              return <li key={i.toString()}>{item}</li>
+             })
+            }
+       </ul>
       
-      {(
-        ()=>{
-          return "Hellow"
-        }
-      )()}
-
     </div>
   );
 };
