4313e079c7594b669f5e3b8749f7601cc08f35f7
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-20 10:18:00 +0600

    react hook useref change value and attribute

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,21 @@ import { useRef } from "react";
 
   const App = () =>{
     
-  let myHeadLine= useRef();
+  let myImage= useRef();
 
   const change =()=>{
-  // myHeadLine.current.innerText="Hellow UseRef";
-  // myHeadLine.current.innerHTML="<ul><li>A</li><li>B</li></ul>";
 
-  // when not using current 
-  myHeadLine.innerHTML="<ul><li>A</li><li>B</li></ul>";
+  myImage.current.src="https://placehold.co/600x400?text=Hello\nWorld";
+  myImage.current.setAttribute('height','200px');
+  myImage.current.setAttribute('width','300px');
 
   }
 
   return(
         <div>
-          {/* using with current method  */}
-          {/* <h1 ref={myHeadLine}></h1> */}
-          
           {/* when not using current  */}
-          <h1 ref={(h1)=>myHeadLine=h1}></h1>
+
+          <img ref={myImage} src="https://placehold.co/600x400" alt="" />
           <button onClick={change}>Click</button>
         </div>
   );
