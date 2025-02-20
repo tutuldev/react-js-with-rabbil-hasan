5cfad7082b5e97b965283cb15c751af2a4b58ac0
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-20 08:34:11 +0600

    passing simple data with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
+import Hero from "./components/Hero";
 
 
-// immdeitely invock condictional rangering 
 
 const App = () => {
 
-  let status = false;
 
   return(
         <div>
-            <h1>Login Status</h1>
-          {(()=>{
-            if(status ==true){
-                  return <button>Logout</button>
-                  }else{
-                  return <button>Login</button>
-                  }
-          })()}
+         <Hero title="Learn React" des="In details how to pass props"/>
+         
         </div>
   )
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
-const Hero = () => {
+const Hero = (props) => {
     return (
         <div>
-            <img src="https://rabbil.com/files/rabbilVai1.webp?v=1" alt="" height="150px"/>
+            {/* <img src="https://rabbil.com/files/rabbilVai1.webp?v=1" alt="" height="150px"/> */}
+          <h1> {props.title}</h1>
+          <p>{props.des}</p>
         </div>
     );
 };
